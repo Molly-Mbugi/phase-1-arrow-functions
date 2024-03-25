2a3e3ce7340278c32048ce2a378e5542944a571b
molly <mollynzembi@gmail.com>
2024-03-25 17:40:21 +0300

    creates index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+function divide(){
+    const num1=2000
+    let num2=100
+return (num1/num2);
+}
+const square = (parameter1) => {
+    return parameter1 * parameter1;
+};
+const add = (num1,num2) => {
+return num1+num2;
+};
